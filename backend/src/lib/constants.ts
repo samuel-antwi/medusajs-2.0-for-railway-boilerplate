640c0a04b40defe6aa62aac8397b53f8878e87a9
Samuel Antwi <samuelantwidev@gmail.com>
2025-04-18 21:48:55 +0100

    add paystack

--- backend/src/lib/constants.ts
+++ backend/src/lib/constants.ts
@@ -1,31 +1,36 @@
-import { loadEnv } from '@medusajs/framework/utils'
+import { loadEnv } from "@medusajs/framework/utils";
 
-import { assertValue } from 'utils/assert-value'
+import { assertValue } from "utils/assert-value";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 /**
  * Is development environment
  */
-export const IS_DEV = process.env.NODE_ENV === 'development'
+export const IS_DEV = process.env.NODE_ENV === "development";
 
 /**
  * Public URL for the backend
  */
-export const BACKEND_URL = process.env.BACKEND_PUBLIC_URL ?? process.env.RAILWAY_PUBLIC_DOMAIN_VALUE ?? 'http://localhost:9000'
+export const BACKEND_URL =
+  process.env.BACKEND_PUBLIC_URL ??
+  process.env.RAILWAY_PUBLIC_DOMAIN_VALUE ??
+  "http://localhost:9000";
 
 /**
  * Database URL for Postgres instance used by the backend
  */
 export const DATABASE_URL = assertValue(
   process.env.DATABASE_URL,
-  'Environment variable for DATABASE_URL is not set',
-)
+  "Environment variable for DATABASE_URL is not set"
+);
 
 /**
  * (optional) Redis URL for Redis instance used by the backend
  */
 export const REDIS_URL = process.env.REDIS_URL;
+
+export const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
 
 /**
  * Admin CORS origins
@@ -47,16 +52,16 @@ export const STORE_CORS = process.env.STORE_CORS;
  */
 export const JWT_SECRET = assertValue(
   process.env.JWT_SECRET,
-  'Environment variable for JWT_SECRET is not set',
-)
+  "Environment variable for JWT_SECRET is not set"
+);
 
 /**
  * Cookie secret used for signing cookies
  */
 export const COOKIE_SECRET = assertValue(
   process.env.COOKIE_SECRET,
-  'Environment variable for COOKIE_SECRET is not set',
-)
+  "Environment variable for COOKIE_SECRET is not set"
+);
 
 /**
  * (optional) Minio configuration for file storage
@@ -94,9 +99,13 @@ export const MEILISEARCH_ADMIN_KEY = process.env.MEILISEARCH_ADMIN_KEY;
  * Worker mode
  */
 export const WORKER_MODE =
-  (process.env.MEDUSA_WORKER_MODE as 'worker' | 'server' | 'shared' | undefined) ?? 'shared'
+  (process.env.MEDUSA_WORKER_MODE as
+    | "worker"
+    | "server"
+    | "shared"
+    | undefined) ?? "shared";
 
 /**
  * Disable Admin
  */
-export const SHOULD_DISABLE_ADMIN = process.env.MEDUSA_DISABLE_ADMIN === 'true'
+export const SHOULD_DISABLE_ADMIN = process.env.MEDUSA_DISABLE_ADMIN === "true";
